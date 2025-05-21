5625506a4bd396477ec51200e64bf55473bb7133
SebasLzCb <sebas2006loza@gmail.com>
2025-05-21 15:11:46 -0500

    Finalizacion de practica para repaso

--- src/main/ejercicio1/RespuestasComplejidad.java
+++ src/main/ejercicio1/RespuestasComplejidad.java
@@ -1,15 +1,14 @@
 package main.ejercicio1;
 
 public class RespuestasComplejidad {
-    // Los estudiantes deben completar cada variable con la notación Big O correcta
-    public static String imprimirArreglo = ""; // 1
-    public static String busquedaLineal = ""; // 2
-    public static String busquedaBinaria = ""; // 3
-    public static String factorial = ""; // 4
-    public static String fibonacci = ""; // 5
-    public static String bubbleSort = ""; // 6
-    public static String quickSort = ""; // 7
-    public static String sumaMatriz = ""; // 8
-    public static String encontrarMaximo = ""; // 9
-    public static String potencia = ""; // 10
+    public static String imprimirArreglo = "O(n)";
+    public static String busquedaLineal = "O(n)";
+    public static String busquedaBinaria = "O(log n)";
+    public static String factorial = "O(n)";
+    public static String fibonacci = "O(2^n)";
+    public static String bubbleSort = "O(n^2)";
+    public static String quickSort = "O(n log n)";
+    public static String sumaMatriz = "O(n^2)";
+    public static String encontrarMaximo = "O(n)";
+    public static String potencia = "O(n)";
 }
--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -1,30 +1,43 @@
 package main.ejercicio2;
 
-public class RespuestasOptimizadas {
-    // El estudiante debe escribir aquí las versiones optimizadas y las
-    // complejidades
+import java.util.HashSet;
 
-    // 1. Versión optimizada de tieneDuplicados (Complejidad anotada en la variable)
-    public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
+public class RespuestasOptimizadas {
+
+    // 1. Versión optimizada de tieneDuplicados (usa HashSet)
+    public static String tieneDuplicadosComplejidad = "O(n)";
 
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
+        HashSet<Integer> vistos = new HashSet<>();
+        for (int n : arr) {
+            if (vistos.contains(n))
+                return true;
+            vistos.add(n);
+        }
         return false;
     }
 
-    // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
-    public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
+    // 2. Versión optimizada de contarMayores (una sola pasada)
+    public static String contarMayoresComplejidad = "O(n)";
 
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int count = 0;
+        for (int n : arr) {
+            if (n > x)
+                count++;
+        }
+        return count;
     }
 
-    // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
-    public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
+    // 3. Versión optimizada de encontrarMaximo (una sola pasada)
+    public static String encontrarMaximoComplejidad = "O(n)";
 
     public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+        int max = arr[0];
+        for (int n : arr) {
+            if (n > max)
+                max = n;
+        }
+        return max;
     }
 }
